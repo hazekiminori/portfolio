a5c9aa8e58ed6d364c817e577c05d21277b3851d
EC2 Default User <ec2-user@ip-172-31-30-106.ap-northeast-1.compute.internal>
2022-12-09 15:15:47 +0000

    routing途中
    namespaceを使って記載途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,31 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  devise_for :admins,skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root to: "public/homes#top"
+  get '/admin' => 'admin/homes#top'
+  
+  namespace :admin do
+    resources :categories
+    resources :users
+    resources :changes
+    resources :procedures
+    resources :bookmarks
+  end
+  
+  scope module: :public do
+    resources :categories
+    resources :users
+    resources :changes
+    resources :procedures
+    resources :bookmarks
+  end
+  
 end
+
