1a90333dfc573d43efd7f42d7137e04a0c86219c
EC2 Default User <ec2-user@ip-172-31-30-106.ap-northeast-1.compute.internal>
2022-12-10 14:50:43 +0000

    職員番号でログイン準備
    マイグレーションファイルにstaff_numberを追加

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2022_12_09_132438) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "staff_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
@@ -91,9 +92,9 @@ ActiveRecord::Schema.define(version: 2022_12_09_132438) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "staff_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "is_deleted"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
