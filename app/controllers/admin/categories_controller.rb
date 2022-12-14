c6e6f22ccb50e57623fe6a45014312b6a3fc3e83
EC2 Default User <ec2-user@ip-172-31-30-106.ap-northeast-1.compute.internal>
2022-12-14 12:31:12 +0000

    カテゴリーコントローラーの記載

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,36 @@
 class Admin::CategoriesController < ApplicationController
+  
+  def index
+    @categories = Category.all
+  end
+  
+  def show
+    @category = Category.find(params[:id])
+  end
+  
+  def create
+    @category = category.new
+    @category.save
+    redirect_to categories_path
+  end
+  
+  def edit
+    @category = Category.find(params[:id])
+  end
+  
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to categories_path
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def category_params
+    params.require(:category).permit(:name)
+  end
+  
 end
