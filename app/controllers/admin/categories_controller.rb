7bd49e6c4861561e4cfbbc71eb42cd2511332e36
EC2 Default User <ec2-user@ip-172-31-30-106.ap-northeast-1.compute.internal>
2022-12-14 17:44:41 +0000

    admin Userコントローラー記載

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -21,7 +21,7 @@ class Admin::CategoriesController < ApplicationController
   def update
     @category = Category.find(params[:id])
     if @category.update(category_params)
-      redirect_to categories_path
+      redirect_to admin_categories_path
     else
       render :edit
     end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,4 +4,20 @@ class Admin::UsersController < ApplicationController
     @user = User.all
   end
   
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to admin_users_path
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:is_deleted)
+  end
+
 end
