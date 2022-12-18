8473ca7d1f96cb792c43020fcc000b15e82b82c8
EC2 Default User <ec2-user@ip-172-31-30-106.ap-northeast-1.compute.internal>
2022-12-18 16:05:52 +0000

    利用者の編集ページ、退会画面作成

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,2 +1,12 @@
 class Public::UsersController < ApplicationController
+  
+  def edit
+  end
+  
+  def update
+    @user = current_user
+    @user.update
+    redirect_to my_page_path
+  end
+  
 end
